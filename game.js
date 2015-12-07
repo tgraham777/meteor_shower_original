dbafb62b8641b7cf6cc2838236080d9901ef541f
Tyler Graham <tylergraham134@gmail.com>
2015-12-07 15:10:51 -0700

    implements horizontal movement functionality

--- game.js
+++ game.js
@@ -18,10 +18,13 @@
 
   Game.prototype = {
     update: function() {
-
+      for (var i = 0; i < this.bodies.length; i++) {
+        this.bodies[i].update();
+      }
     },
 
     draw: function(screen, gameSize) {
+      screen.clearRect(0, 0, gameSize.x, gameSize.y);
       for (var i = 0; i < this.bodies.length; i++) {
          drawRect(screen, this.bodies[i]);
       }
@@ -29,13 +32,19 @@
   };
 
   var Player = function(game, gameSize) {
+    this.game = game;
     this.size = { x: 15, y: 15 };
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
   };
 
   Player.prototype = {
     update: function() {
-
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
     }
   };
 
@@ -43,6 +52,24 @@
     screen.fillRect(body.center.x - body.size.x / 2,
                     body.center.y - body.size.y / 2,
                     body.size.x, body.size.y);
+  };
+
+  var Keyboarder = function() {
+    var keyState = {};
+
+    window.onkeydown = function(e) {
+      keyState[e.keyCode] = true;
+    };
+
+    window.onkeyup = function(e) {
+      keyState[e.keyCode] = false;
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32 };
   };
 
   window.onload = function() {
